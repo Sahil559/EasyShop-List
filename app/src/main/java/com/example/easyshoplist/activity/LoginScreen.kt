19212b9c8848589749b79c96fba5f1f5cb5efb23
Sahil559 <sahusahil064@gmail.com>
2024-02-21 20:26:21 +0530

    Second commit

---
+++ app/src/main/java/com/example/easyshoplist/activity/LoginScreen.kt
@@ -0,0 +1,96 @@
+package com.example.easyshoplist.activity
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import com.example.easyshoplist.Data.AuthViewModel
+import com.example.easyshoplist.Data.Result
+
+
+@Composable
+fun Loginscreen(
+    authViewModel: AuthViewModel,
+    onNavigatetoSignup :()-> Unit,
+    onSignInSuccess :()-> Unit
+
+) {
+    var email by remember { mutableStateOf("") }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+    val result by authViewModel.authResult.observeAsState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(
+            onClick = {
+                authViewModel.login(email, password)
+                when(result){
+                    is Result.Success->{
+                        onSignInSuccess()
+                    }
+                    is Result.Error ->{
+
+                    }
+
+                    else -> {
+
+                    }
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Login")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Don't have an account? Sign up.",
+            modifier = Modifier.clickable { onNavigatetoSignup() }
+        )
+    }
+}
